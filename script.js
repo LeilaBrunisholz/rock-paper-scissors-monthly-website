57e6eef671831ce95d7965f0dd032e5b412e4a3b
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-02-26 18:19:27 +0000

    rock done

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let userName = prompt('enter your name')
 let names = `Hello, ${userName}, prepare to lose.`
+document.getElementById('userWins').textContent = `${userName} wins = 0`
 let userNameOutputDiv = document.getElementById('userNameOutput')
 let computerChoice
+let compScore = 0
+let userScore = 0
+let ties = 0
 
 
 console.log(names)
@@ -12,48 +16,70 @@ function playRound(userSelection){
      userChoice = userSelection
     //Get Computer Choice
     const randomNumber = Math.floor(Math.random()*3);
-    if (randomNumber === 1) {
+    if (randomNumber === 0) {
         computerChoice = 'Rock'
+        console.log(randomNumber)
     }
-    else if (randomNumber === 2) {
+    else if (randomNumber === 1) {
         computerChoice = 'Paper'
+        console.log(randomNumber)
     }
     else  {
         computerChoice = 'Scissor'
+        console.log(randomNumber)
     }
  //Display Selections
  document.getElementById("comp_selection").textContent = computerChoice
  document.getElementById("user_selection").textContent = userChoice
 
  //Evaluate the winner
+//USer Chooses Rock
+ if (userChoice === 'Rock' && computerChoice === 'Rock'){
+    console.log('Tie')
+    ties++   
+    console.log(userScore)
+} else if (userChoice === 'Rock' && computerChoice === 'Paper'){
+    console.log('Computer Wins')
+    compScore++
+    console.log(userScore)
+} else {
+    console.log('You win')
+    userScore++
+    console.log(userScore)
+} 
+//user chooses Paper
+if (userChoice === 'Paper' && computerChoice === 'Rock'){
+    console.log('Tie')
+    ties++   
+} else if (userChoice === 'Paper' && computerChoice === 'Paper'){
+    console.log('Computer Wins')
+    compScore++
+} else {
+    console.log('You win')
+    userScore++
+   
+} 
 
- 
-}
+
+//update visuals on site
+document.getElementById('userWins').textContent = `${userName} wins = ${userScore}`
+document.getElementById('compWins').textContent = `Computer wins = ${compScore}`
+document.getElementById('ties').textContent = `Ties = ${ties}`
+
+
+} 
+
+
    
 
 
-const randomNumber = Math.floor(Math.random()*3);
-let answer, image, color;
-
-
-
-const responseText = document.getElementById('response-text');
-responseText.innerText = answer;
-responseText.classList = `display-4 ${color}`;
-document.getElementById('response-image').src = image;
 
 
 
 
 
 
-if (userChoice === 'Rock' && computerChoice === 'Rock'){
-    console.log('Tie')
-} else {
-    console.log('stop')
-}
 
-//in order for this to show up you have to assign the ties an id so that it could change the number 
 
 
 
