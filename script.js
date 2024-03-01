a1bd54a31329adc1aec24736594c13a41997bde6
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-03-01 14:44:50 +0000

    commit

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ let ties = 0
 let rounds = 0
 
 
-console.log(names)
 userNameOutputDiv.innerHTML = `<p>${names}</p>`
 //user name// 
 
-function playRound(userSelection){
-     userChoice = userSelection
+function playRound(userChoice) {
+    
     //Get Computer Choice
-    const randomNumber = Math.floor(Math.random()*3);
+    const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         computerChoice = 'Rock'
         console.log(randomNumber)
@@ -25,87 +24,78 @@ function playRound(userSelection){
         computerChoice = 'Paper'
         console.log(randomNumber)
     }
-    else  {
+    else {
         computerChoice = 'Scissor'
         console.log(randomNumber)
     }
- //Display Selections
- document.getElementById("comp_selection").textContent = computerChoice
- document.getElementById("user_selection").textContent = userChoice
+    //Display Selections
+    document.getElementById("comp_selection").textContent = computerChoice
+    document.getElementById("user_selection").textContent = userChoice
 
- //Evaluate the winner
-//USer Chooses Rock
- if (userChoice === 'Rock' && computerChoice === 'Rock'){
-    console.log('Tie')
-    ties++   
-    console.log(userScore)
-} else if (userChoice === 'Rock' && computerChoice === 'Paper'){
-    console.log('Computer Wins')
-    compScore++
-    console.log(userScore)
-} else if(userChoice === 'Rock' && computerChoice === 'Scissor'){
-    console.log('You win')
-    userScore++
-    console.log(userScore)
-}
-//user chooses Paper
-else if (userChoice === 'Paper' && computerChoice === 'Paper'){
-    console.log('Tie')
-    ties++   
-    console.log(userScore)
-} else if (userChoice === 'Paper' && computerChoice === 'Scissor'){
-    console.log('Computer Wins')
-    compScore++
-    console.log(userScore)
-} else if(userChoice === 'Paper' && computerChoice === 'Rock'){
-    console.log('You win')
-    userScore++
-    console.log(userScore)
-}
+    //Evaluate the winner
+    //USer Chooses Rock
+    if (userChoice === computerChoice) {
+        console.log('Tie')
+        ties++
+        console.log(userScore)
+    } else if (userChoice === 'Rock' && computerChoice === 'Paper') {
+        console.log('Computer Wins')
+        compScore++
+        console.log(userScore)
+    } else if (userChoice === 'Rock' && computerChoice === 'Scissor') {
+        console.log('You win')
+        userScore++
+        console.log(userScore)
+    }
+    //user chooses Paper
+    else if (userChoice === 'Paper' && computerChoice === 'Scissor') {
+        console.log('Computer Wins')
+        compScore++
+        console.log(userScore)
+    } else if (userChoice === 'Paper' && computerChoice === 'Rock') {
+        console.log('You win')
+        userScore++
+        console.log(userScore)
 
-    //user chooses Scissor
-else if (userChoice === 'Scissor' && computerChoice === 'Scissor'){
-    console.log('Tie')
-    ties++   
-    console.log(userScore)
-} else if (userChoice === 'Scissor' && computerChoice === 'Rock'){
-    console.log('Computer Wins')
-    compScore++
-    console.log(userScore)
-} else if(userChoice === 'Scissor' && computerChoice === 'Paper'){
-    console.log('You win')
-    userScore++
-    console.log(userScore)
-}
+    } else if (userChoice === 'Scissor' && computerChoice === 'Rock') {
+        console.log('Computer Wins')
+        compScore++
+        console.log(userScore)
+    } else if (userChoice === 'Scissor' && computerChoice === 'Paper') {
+        console.log('You win')
+        userScore++
+        console.log(userScore)
+    }
+rounds = rounds +1
+displayResults();
+checkEndGame();
 
+    }
 
-else if(userChoice === 'Rock' || userChoice === 'Scissor' || userChoice === 'Paper'){
-    console.log('round 1')
-    rounds++
-    console.log(rounds)
-}
-
-else if (Round === 5){
-    console.log('Game over')
-} else if (compScore>userScore){
-    console.log('computer wins')
-} else if (compScore<userScore){
-    console.log ('you win')
-} else {
-    console.log ('tie')
+function displayResults(){
+    
+    //update visuals on site
+    document.getElementById('userWins').textContent = `${userName} wins = ${userScore}`
+    document.getElementById('compWins').textContent = `Computer wins = ${compScore}`
+    document.getElementById('ties').textContent = `Ties = ${ties}`
+    document.getElementById('rounds').textContent = `Rounds = ${rounds}`
 }
 
 
-//update visuals on site
-document.getElementById('userWins').textContent = `${userName} wins = ${userScore}`
-document.getElementById('compWins').textContent = `Computer wins = ${compScore}`
-document.getElementById('ties').textContent = `Ties = ${ties}`
+function checkEndGame(){
 
+    if (rounds >= 5) {
+        console.log('Game over')
+        if (compScore > userScore) {
+            console.log('computer wins')
+        } else if (compScore < userScore) {
+            console.log('you win')
+        } else {
+            console.log('No one wins')
+        }
+    } 
 
-} 
-
-
-   
+}
 
 
 
